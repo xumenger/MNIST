a812c2541b447886a9ba974b5d018506f2a57eb0
xumenger <xumenger@126.com>
2018-05-28 13:37:08 +0800

    define convolutional model

--- src/mnist/convolutional.py
+++ src/mnist/convolutional.py
@@ -4,3 +4,12 @@ import tensorflow as tf
 import input_data
 
 data = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+'''定义模型
+'''
+with tf.variable_scope("convolutional"):
+    x = tf.placeholder(tf.float32, [None, 784], name='x')
+    keep_prob = tf.placeholder(tf.float32)
+    y, variables = model.convolutional(x, keep_prob)
+
+
